6ea50ff80f965fad4993337c1f9adf40a5800d07
Mohammad Reza Kardoost <mrk74@outlook.com>
2020-02-06 16:12:30 +0330

    create project

---
+++ app/src/main/java/com/github/RezaKardoost/shirazuinternet/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.RezaKardoost.shirazuinternet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
